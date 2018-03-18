5c57345b6e93f377be41162a3cba575601407fb9
Steven Inouye <steveninouye@msn.com>
2018-03-17 19:41:36 -1000

    serving my public directory and also my handlebars

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const hbs = require('hbs');
+const bodyParseer = require('body-parser');
+const app = express();
+const PORT = 8080;
+
+app.set('view engine', 'hbs');
+app.engine('hbs', hbs.__express);
+hbs.registerPartials(__dirname + '/views/partials');
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.render('index', { hello: 'hi' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server up on port ${PORT}`);
+});
